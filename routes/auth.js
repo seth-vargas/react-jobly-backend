aabf5f3d841b6d69d250e109a93af015151eb7cd
Seth Vargas <shvargas01@gmail.com>
2023-11-19 00:34:38 -0600

    Minor changes

--- routes/auth.js
+++ routes/auth.js
@@ -23,19 +23,18 @@ router.post("/token", async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userAuthSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
     const { username, password } = req.body;
     const user = await User.authenticate(username, password);
     const token = createToken(user);
-    return res.json({ token });
+    return res.json({ token, user });
   } catch (err) {
     return next(err);
   }
 });
-
 
 /** POST /auth/register:   { user } => { token }
  *
@@ -50,17 +49,17 @@ router.post("/register", async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userRegisterSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
-    const newUser = await User.register({ ...req.body, isAdmin: false });
-    const token = createToken(newUser);
-    return res.status(201).json({ token });
+    const user = await User.register({ ...req.body, isAdmin: false });
+    const token = createToken(user);
+
+    return res.status(201).json({ token, user });
   } catch (err) {
     return next(err);
   }
 });
-
 
 module.exports = router;
